592b34a38a09d5ccb6837fb2b00cbf511be955c5
Agustin Pietrasanta <aguspietra@gmail.com>
2018-04-01 11:01:53 -0300

    Hecho que el post pueda recibir un texto y devolverlo.

--- src/main/java/hello/RegardsController.java
+++ src/main/java/hello/RegardsController.java
@@ -9,8 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RegardsController {
 
+    private static final String template = "Hello, %s!";
     @RequestMapping(value="/regards",method=RequestMethod.POST)
-    public Regards regards() {
-        return new Regards("Same to you!");
+    public Regards regards(@RequestParam(value="sender", defaultValue="Unknown") String sender) {
+        return new Regards( String.format(template, sender));
     }
 }
