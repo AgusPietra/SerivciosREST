18088c6a41ff13d4a468a8a72cd0c8a4744bead4
Agustin Pietrasanta <aguspietra@gmail.com>
2018-04-23 22:58:26 -0300

    Actualizada clase Code en backend.

---
+++ Back-end/src/main/java/com/rb/module/common/response/codes/Code.java
@@ -0,0 +1,14 @@
+package com.rb.module.common.response.codes;
+
+public class Code {
+
+    private int code;
+
+    public Code(int code){
+        this.code = code;
+    }
+
+    public int getCode() {
+        return this.code;
+    }
+}
